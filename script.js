c6de7cd9a229347b348d7ed04fc8acb2cd4caf81
Amritansh Raizada <amritanshspc@gmail.com>
2025-04-20 00:04:52 +0530

    FINAL HOME PAGE

--- script.js
+++ script.js
@@ -173,7 +173,18 @@ dropdown.addEventListener("mouseenter", () => {
 dropdown.addEventListener("mouseleave", () => {
   backdrop.style.display = "none";
 });
+var sections = document.querySelectorAll(".sec-right")
 
+    sections.forEach(function (elem) {
+        elem.addEventListener("mouseenter", function () {
+            elem.childNodes[3].style.opacity = 1
+            elem.childNodes[3].play()
+        })
+        elem.addEventListener("mouseleave", function () {
+            elem.childNodes[3].style.opacity = 0
+            elem.childNodes[3].load()
+        })
+    })
 
 // Init Everything
 circleChaptaKaro();
